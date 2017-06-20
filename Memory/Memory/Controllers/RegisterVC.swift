bf3313488cf04de4ebdbc4756153bd52c0d4185b
george <george-mamardashvili@yandex.ru>
2017-06-20 19:07:13 +0300

    empty project

---
+++ Memory/Memory/Controllers/RegisterVC.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterVC.swift
+//  Memory
+//
+//  Created by Георгий Мамардашвили on 20.06.17.
+//  Copyright © 2017 Георгий Мамардашвили. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
